5ecd3a7fe1148397ae3bc76e8596bf7b20aed4ce
Javitocatral <javitocatral@gmail.com>
2023-04-26 17:48:29 +0200

    main

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["svelte3", "@typescript-eslint"],
+  ignorePatterns: ["*.cjs"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    "array-callback-return": "error",
+    "prefer-destructuring": "error",
+    "no-return-await": "error",
+    "require-await": "error",
+    "no-undef": "off",
+    "no-var": "error",
+    "prefer-template": "error",
+    "no-else-return": "error",
+    "template-curly-spacing": "error",
+    "one-var": ["error", "never"],
+    eqeqeq: "error",
+    "object-shorthand": "error",
+    "no-unneeded-ternary": "error",
+    "quote-props": ["error", "as-needed"],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          Function: false,
+        },
+      },
+    ],
+  },
+};
