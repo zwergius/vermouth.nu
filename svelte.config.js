bbba1e009670575d06f4fd99bc76fe026e4afae3
Javitocatral <javitocatral@gmail.com>
2023-04-27 09:42:48 +0200

    fallo

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,33 @@ import { vitePreprocess } from "@sveltejs/kit/vite"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    //  By default, `npm run build` will create a standard Node app.
+    //  You can create optimized builds for different platforms by
+    //  specifying a different adapter
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: false,
+      strict: true,
+    }),
+
+    prerender: {
+      crawl: true,
+      //enabled: true,
+      // onError: ({ status, path, referrer, referenceType }) => {
+      //   if (path.startsWith('/blog')) throw new Error('Missing a blog page!');
+      //   console.warn(`${status} ${path}${referrer ? ` (${referenceType} from ${referrer})` : ''}`);
+      // },
+    },
   },
 }
 
