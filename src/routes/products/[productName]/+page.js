ec1588589b6a5a65540d940365071c67259800df
Javitocatral <javitocatral@gmail.com>
2023-08-17 15:36:26 +0200

    Unexpected token

--- src/routes/products/[productName]/+page.js
+++ src/routes/products/[productName]/+page.js
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit'
 import { vermouths } from '../data.js'
 
+/** @type {import('./$types').PageLoad} */
 export function load({ params }) {
   const vermouth = vermouths.find((vermouth) => vermouth.name === params.productName)
 
