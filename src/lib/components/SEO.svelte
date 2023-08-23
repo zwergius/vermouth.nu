84de130419d376a43a85244920709eb7b9efbe1e
Javitocatral <javitocatral@gmail.com>
2023-08-23 10:01:39 +0200

    image change

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -3,7 +3,9 @@
   export let description = 'Spansk vermouth-distributør i premium drinks til en god pris'
   export let tags = ['vermouth', 'spansk', 'fremragende varme', 'alkohol', 'premiun']
   export let ogType = 'www.vermouth.nu'
-  export let image = '/Users/javiergascon/git/vermouth-nu/src/lib/assets/banner.webp'
+  export let image =
+    'https://vermouth.nu/cdn/shop/files/552A24A2-C31B-4C91-A8B3-5A3C5F5EA8D3_300x300.JPG?v=1615928417'
+  export let altImage = 'Forzudo er et af eksemplerne på vermouth, som du kan finde'
 </script>
 
 <svelte:head>
@@ -25,6 +27,6 @@
   <meta name="twitter:url" content={ogType} />
   {#if image}
     <meta name="twitter:image" content={image} />
-    <meta name="twitter:image:alt" content={title} />
+    <meta name="twitter:image:alt" content={altImage} />
   {/if}
 </svelte:head>
