4e0678aee0e0021aa026534b8f09e4d303ef2e70
Javitocatral <javitocatral@gmail.com>
2023-09-04 16:13:34 +0200

    fix social media seo

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -19,18 +19,18 @@
   <!-- <meta name="twitter:card" content="summary_large_image" /> -->
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:url" content="https://www.vermouth.nu" />
+  <meta name="twitter:url" content={ogType} />
+  <meta name="twitter:image" content={image} />
   <meta name="twitter:image:alt" content={imageAlt} />
 
   <!--  Essential META Tags -->
 
-  <meta property="”og:locale”" content="" />
-  <meta property="”fb:app_id”" content="" />
+  <meta property="”og:locale”" content="””" />
+  <meta property="”fb:app_id”" content="””" />
   <meta property="og:site_name" content="Vermouth.NU" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
   <meta property="og:image" itemprop="image" content={image} />
-  <meta itemprop="image" content={image} />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="www.vermouth.nu" />
 </svelte:head>
