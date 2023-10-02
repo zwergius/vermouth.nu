5e017b9231aa0ef82f2434ae088a78d844c32b85
Javitocatral <javitocatral@gmail.com>
2023-10-02 17:46:00 +0200

    redirecting passes

---
+++ src/routes/cards/[cardsName]/types.d.ts
@@ -0,0 +1,10 @@
+export enum CardNames {
+  Christian = 'christian',
+  Christoffer = 'christoffer',
+  Thomas = 'thomas',
+}
+
+export interface CardData {
+  passGoogle: string
+}
+export default CardNames
