78c96d8f7263b7917e83d0353a227ccb11003754
Javitocatral <javitocatral@gmail.com>
2023-10-23 12:55:39 +0200

    test images, url, links, etc..

---
+++ tests/old-url.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/christian-vermouth-nu.pkpass')
+  await page.waitForTimeout(2000)
+  const userAgent = await page.evaluate(() => navigator.userAgent)
+  const currentURL = page.url()
+  if (userAgent.includes('Chrome') || userAgent.includes('Firefox')) {
+    expect(currentURL).toBe('http://localhost:5173/cards/christian')
+  }
+
+  if (userAgent.includes('safari')) {
+    expect(currentURL).toBe('http://localhost:5173/christian.pkpass')
+  }
+})
