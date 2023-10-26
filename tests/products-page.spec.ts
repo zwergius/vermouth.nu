ecb30e5eee9d01610a196e317b7c8748195e751a
Javitocatral <javitocatral@gmail.com>
2023-10-26 16:17:00 +0200

    pr fix comment

--- tests/products-page.spec.ts
+++ tests/products-page.spec.ts
@@ -2,9 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('products page should contain an image in the link', async ({ page }) => {
   await page.goto(`/products`)
-  const link = await page.getByRole('link', {
-    name: 'Forzudo Blanco Forzudo Blanco Bierzo, Leon, Nordvest Spanien 100 cl. / 15%',
-  })
+  const link = await page.locator('a').first()
   const image = await link.locator('img').first()
   expect(image).toBeTruthy()
 })
