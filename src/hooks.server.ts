7b4f4a1985aa99ae56a50ee6796de5617eb2724e
zwergius <webmaster@zwergius.com>
2026-03-09 10:22:19 +0100

    Improve locale derivation and fix currency to DKK

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,28 +2,19 @@ import { PUBLIC_MEDUSA_PUBLISHABLE_KEY, PUBLIC_VITE_BACKEND_URL } from '$env/sta
 import type { Handle, HandleFetch } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
+  let locale = 'en'
   const lang = event.request.headers.get('accept-language')?.split(',')[0]
   if (lang) {
-    event.locals.locale = lang
+    ;[locale] = lang.split(';')
   }
+  event.locals.locale = locale
   return resolve(event)
 }
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-  console.log(
-    'event.url.origin: ',
-    event.url.origin,
-    ' request.url: ',
-    request.url,
-    'PUBLIC_VITE_BACKEND_URL: ',
-    PUBLIC_VITE_BACKEND_URL,
-  )
   if (request.url.includes('/store/')) {
-    console.log('replacedUrl: ', request.url.replace(event.url.origin, PUBLIC_VITE_BACKEND_URL))
     request = new Request(request.url.replace(event.url.origin, PUBLIC_VITE_BACKEND_URL), request)
     request.headers.set('x-publishable-api-key', PUBLIC_MEDUSA_PUBLISHABLE_KEY)
   }
-
-  console.log(JSON.stringify(request))
   return fetch(request)
 }
--- src/lib/helpers/numbers.ts
+++ src/lib/helpers/numbers.ts
@@ -1,3 +1,7 @@
 export function formatPrice(amount: number, currency: string, locale: string): string {
-  return new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount)
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    currencyDisplay: 'code',
+  }).format(amount)
 }
