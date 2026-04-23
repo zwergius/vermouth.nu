99a8778e5e6ed61aaca9dc36c6fcee4834c24705
zwergius <webmaster@zwergius.com>
2026-04-23 14:44:28 +0200

    force cart refresh in order page if needed

--- src/routes/orders/[id]/+page.server.ts
+++ src/routes/orders/[id]/+page.server.ts
@@ -1,8 +1,10 @@
 import type { PageServerLoad } from './$types'
 import { sdk } from '$lib/medusa'
 
-export const load: PageServerLoad = async ({ params, locals }) => {
+export const load: PageServerLoad = async ({ params, parent, locals }) => {
   const { id } = params
+  // Calling await parent() to make sure cart is renewed
+  await parent()
   const { order } = await sdk.store.order.retrieve(id)
 
   return {
