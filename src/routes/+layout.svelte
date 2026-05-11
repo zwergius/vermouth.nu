8d0724bd2a1746edd915d93b2d4eb2f6ea8401c1
zwergius <webmaster@zwergius.com>
2026-05-11 10:47:55 +0200

    adds google tag manager

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,6 +32,22 @@
     </script>
     <!-- End cookieyes banner -->
   {/if}
+  {#if true}
+    <!-- Google Tag Manager -->
+    <script>
+      ;(function (w, d, s, l, i) {
+        w[l] = w[l] || []
+        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : ''
+        j.async = true
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+        f.parentNode.insertBefore(j, f)
+      })(window, document, 'script', 'dataLayer', 'GTM-PL5P58FS')
+    </script>
+    <!-- End Google Tag Manager -->
+  {/if}
   {#if PUBLIC_GA_MEASUREMENT_ID}
     <script
       async
