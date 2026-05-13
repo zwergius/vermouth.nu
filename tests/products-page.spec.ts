ef825a21ff306fab2a93cdcda767be581977d9e3
zwergius <webmaster@zwergius.com>
2026-05-13 13:15:53 +0200

    Add current integration coverage

--- tests/products-page.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('products page should contain an image in the link', async ({ page }) => {
-  await page.goto(`/products`)
-  const link = await page.locator('a').first()
-  const image = await link.locator('img').first()
-  expect(image).toBeTruthy()
-})
