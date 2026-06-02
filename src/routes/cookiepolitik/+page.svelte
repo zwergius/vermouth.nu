1a36425a7ad0d121f5929e7c58b38694e19cc21a
zwergius <webmaster@zwergius.com>
2026-06-03 00:02:39 +0200

    adds cookiepolicy

--- src/routes/cookiepolitik/+page.svelte
+++ src/routes/cookiepolitik/+page.svelte
@@ -1,3 +1,297 @@
 <section class="policy">
-  <div id="cky-auto-cookie-policy"></div>
+  <!-- Start CookieYes cookie policy -->
+  <h1 class="cookie-policy-h1">Cookie Policy</h1>
+  <div class="cookie-policy-date-container">
+    <p>Effective date: June 02, 2026</p>
+    <p>Last updated: June 02, 2026</p>
+  </div>
+  &nbsp;
+  <h2>What are cookies?</h2>
+  <div class="cookie-policy-p">
+    <p>
+      This Cookie Policy explains what cookies are, how we use them, the types of cookies we use
+      (i.e., the information we collect using cookies and how that information is used), and how to
+      manage your cookie settings.
+    </p>
+    <p>
+      Cookies are small text files used to store small pieces of information. They are stored on
+      your device when a website loads in your browser. These cookies help ensure that the website
+      functions properly, enhance security, provide a better user experience, and analyse
+      performance to identify what works and where improvements are needed.
+    </p>
+  </div>
+  &nbsp;
+  <h2>How do we use cookies?</h2>
+  <div class="cookie-policy-p">
+    <p>
+      Like most online services, our website uses both first-party and third-party cookies for
+      various purposes. First-party cookies are primarily necessary for the website to function
+      properly and do not collect any personally identifiable data.
+    </p>
+    <p>
+      The third-party cookies used on our website primarily help us understand how the website
+      performs, track how you interact with it, keep our services secure, deliver relevant
+      advertisements, and enhance your overall user experience while improving the speed of your
+      future interactions with our website.
+    </p>
+  </div>
+  &nbsp;
+  <h2>Types of cookies we use</h2>
+  <div class="cky-audit-table-element">
+    <div>
+      <h3>Necessary</h3>
+      <div class="cky-category-des">
+        <p>
+          Necessary cookies are required to enable the basic features of this site, such as
+          providing secure log-in or adjusting your consent preferences. These cookies do not store
+          any personally identifiable data.
+        </p>
+      </div>
+      <div class="cky-table-wrapper">
+        <table class="cky-cookie-audit-table">
+          <thead>
+            <tr>
+              <th> Cookie </th>
+              <th> Duration </th>
+              <th> Description </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> cart_id </td>
+              <td> 1 month </td>
+              <td> No description available. </td>
+            </tr>
+
+            <tr>
+              <td> cookieyes-* </td>
+              <td> 1 year </td>
+              <td> CookieYes sets this cookie for consent solution management. </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div>
+      <h3>Functional</h3>
+      <div class="cky-category-des">
+        <p>
+          Functional cookies help perform certain functionalities like sharing the content of the
+          website on social media platforms, collecting feedback, and other third-party features.
+        </p>
+      </div>
+      <div class="cky-table-wrapper">
+        <table class="cky-cookie-audit-table">
+          <thead>
+            <tr>
+              <th> Cookie </th>
+              <th> Duration </th>
+              <th> Description </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> vc </td>
+              <td> 1 year 1 month 4 days </td>
+              <td>
+                The vc cookie is set by addthis.com on sites that allow sharing on social media.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div>
+      <h3>Analytics</h3>
+      <div class="cky-category-des">
+        <p>
+          Analytical cookies are used to understand how visitors interact with the website. These
+          cookies help provide information on metrics such as the number of visitors, bounce rate,
+          traffic source, etc.
+        </p>
+      </div>
+      <div class="cky-table-wrapper">
+        <table class="cky-cookie-audit-table">
+          <thead>
+            <tr>
+              <th> Cookie </th>
+              <th> Duration </th>
+              <th> Description </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> _ga </td>
+              <td> 1 year 1 month 4 days </td>
+              <td>
+                Google Analytics sets this cookie to calculate visitor, session and campaign data
+                and track site usage for the site's analytics report. The cookie stores information
+                anonymously and assigns a randomly generated number to recognise unique visitors.
+              </td>
+            </tr>
+
+            <tr>
+              <td> _ga_* </td>
+              <td> 1 year 1 month 4 days </td>
+              <td> Google Analytics sets this cookie to store and count page views. </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div>
+      <h3>Advertisement</h3>
+      <div class="cky-category-des">
+        <p>
+          Advertisement cookies are used to provide visitors with customised advertisements based on
+          the pages you visited previously and to analyse the effectiveness of the ad campaigns.
+        </p>
+      </div>
+      <div class="cky-table-wrapper">
+        <table class="cky-cookie-audit-table">
+          <thead>
+            <tr>
+              <th> Cookie </th>
+              <th> Duration </th>
+              <th> Description </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> _gcl_au </td>
+              <td> 3 months </td>
+              <td>
+                Google Tag Manager sets this cookie to experiment advertisement efficiency of
+                websites using their services.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  &nbsp;
+  <h2 style="margin-bottom: 20px">Manage cookie preferences</h2>
+  <a class="btn mb-2">Consent Preferences</a><br />
+  <div>
+    <p>
+      You can modify your cookie settings anytime by clicking the 'Consent Preferences' button
+      above. This will allow you to revisit the cookie consent banner and update your preferences or
+      withdraw your consent immediately.
+    </p>
+    <p>
+      Additionally, different browsers offer various methods to block and delete cookies used by
+      websites. You can adjust your browser settings to block or delete cookies. Below are links to
+      support documents on how to manage and delete cookies in major web browsers.
+    </p>
+    <p>
+      Chrome: <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://support.google.com/accounts/answer/32050"
+        >https://support.google.com/accounts/answer/32050</a
+      >
+    </p>
+    <p>
+      Safari: <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://support.apple.com/en-in/guide/safari/sfri11471/mac"
+        >https://support.apple.com/en-in/guide/safari/sfri11471/mac</a
+      >
+    </p>
+    <p>
+      Firefox: <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox?redirectslug=delete-cookies-remove-info-websites-stored&redirectlocale=en-US"
+        >https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox?redirectslug=delete-cookies-remove-info-websites-stored&redirectlocale=en-US</a
+      >
+    </p>
+    <p>
+      Internet Explorer: <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://support.microsoft.com/en-us/topic/how-to-delete-cookie-files-in-internet-explorer-bca9446f-d873-78de-77ba-d42645fa52fc"
+        >https://support.microsoft.com/en-us/topic/how-to-delete-cookie-files-in-internet-explorer-bca9446f-d873-78de-77ba-d42645fa52fc</a
+      >
+    </p>
+    <p>
+      If you are using a different web browser, please refer to its official support documentation.
+    </p>
+  </div>
+  &nbsp;
+  <p class="cookie-policy-p">
+    Cookie Policy generated by <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.cookiepolicygenerator.ai?utm_source=CPG&utm_medium=footer&utm_campaign=UW"
+      >CookieYes - Cookie Policy Generator</a
+    >
+  </p>
+  <!-- End CookieYes cookie policy -->
 </section>
+
+<style>
+  a.cky-banner-element {
+    padding: 8px 30px;
+    background: #f8f9fa;
+    color: #858a8f;
+    border: 1px solid #dee2e6;
+    box-sizing: border-box;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+
+  .cky-table-wrapper {
+    width: 100%;
+    max-width: 100%;
+    overflow: auto;
+  }
+  .cky-cookie-audit-table {
+    font-family: inherit;
+    border-collapse: collapse;
+    width: 100%;
+  }
+  .cky-cookie-audit-table th {
+    background-color: #d9dfe7;
+    border: 1px solid #cbced6;
+  }
+  .cky-cookie-audit-table td {
+    border: 1px solid #d5d8df;
+  }
+  .cky-cookie-audit-table th,
+  .cky-cookie-audit-table td {
+    text-align: left;
+    padding: 10px;
+    font-size: 12px;
+    color: #000000;
+    word-break: normal;
+  }
+  .cky-cookie-audit-table td p {
+    font-size: 12px;
+    line-height: 24px;
+    margin-bottom: 1em;
+  }
+  .cky-cookie-audit-table td p:last-child {
+    margin-bottom: 0;
+  }
+  .cky-cookie-audit-table tr:nth-child(2n + 1) td {
+    background: #f1f5fa;
+  }
+  .cky-cookie-audit-table tr:nth-child(2n) td {
+    background: #ffffff;
+  }
+  .cky-audit-table-element h3 {
+    margin: 35px 0 16px 0;
+  }
+  .cky-audit-table-element .cky-table-wrapper {
+    margin-bottom: 1rem;
+  }
+  .cky-audit-table-element .cky-category-des p {
+    margin-top: 0;
+  }
+</style>
